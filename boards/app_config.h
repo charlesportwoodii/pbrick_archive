88cd71005911b059de519884d8b6720c891daff1
Charles R. Portwood II <charlesportwoodii@erianna.com>
2019-04-10 21:36:26 -0500

    Adjusting memory regions

--- boards/app_config.h
+++ boards/app_config.h
@@ -19,6 +19,22 @@ extern "C" {
 #define NRF_LOG_BACKEND_SERIAL_USES_UART 1
 #endif
 
+#ifndef NRF_SDH_BLE_LOG_LEVEL
+#define NRF_SDH_BLE_LOG_LEVEL 4
+#endif
+
+#ifndef NRF_SDH_BLE_LOG_ENABLED
+#define NRF_SDH_BLE_LOG_ENABLED 1
+#endif
+
+#ifndef NRF_SDH_LOG_ENABLED
+#define NRF_SDH_LOG_ENABLED 1
+#endif
+
+#ifndef NRF_SDH_LOG_LEVEL
+#define NRF_SDH_LOG_LEVEL 4
+#endif
+
 #endif // PBRICK_DEBUG
 
 #ifdef __cplusplus
